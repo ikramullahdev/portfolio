20d31a96c089a036e4e791c47a7fbd77444b6d73
Muhammad ikram ullah <ikramahmed12201@gmail.com>
2026-07-27 05:54:15 -0700

    Translate quiz questions from Hindi to English

--- quiz-app/script.js
+++ quiz-app/script.js
@@ -1,40 +1,106 @@
 const questions = [
 
 {
-question:"HTML ka full form kya hai?",
+question:"What does HTML stand for?",
 options:[
 "Hyper Text Markup Language",
 "High Text Machine Language",
-"Hyper Tool Multi Language",
-"Home Text Markup Language"
+"Hyperlink Text Management Language",
+"Home Tool Markup Language"
 ],
 answer:0
 },
 
 {
-question:"CSS ka use kis liye hota hai?",
+question:"Which language is used for styling web pages?",
 options:[
-"Database",
-"Styling Website",
-"Programming",
-"Server"
+"HTML",
+"CSS",
+"Java",
+"SQL"
 ],
 answer:1
 },
 
 {
-question:"JavaScript kis type ki language hai?",
+question:"Which tag is used to create a link in HTML?",
 options:[
-"Programming Language",
-"Database",
-"Operating System",
-"Browser"
+"<link>",
+"<a>",
+"<href>",
+"<url>"
+],
+answer:1
+},
+
+{
+question:"Which property changes text color in CSS?",
+options:[
+"background",
+"font-style",
+"color",
+"text-color"
+],
+answer:2
+},
+
+{
+question:"JavaScript is mainly used for?",
+options:[
+"Database creation",
+"Web page interactivity",
+"Operating system",
+"Image editing"
+],
+answer:1
+},
+
+{
+question:"Which symbol is used for comments in JavaScript?",
+options:[
+"//",
+"##",
+"<!-- -->",
+"**"
 ],
 answer:0
 },
 
 {
-question:"JavaScript file ka extension kya hota hai?",
+question:"Which HTML tag is used for the largest heading?",
+options:[
+"<h6>",
+"<heading>",
+"<h1>",
+"<head>"
+],
+answer:2
+},
+
+{
+question:"Which CSS property controls element spacing inside?",
+options:[
+"margin",
+"padding",
+"border",
+"width"
+],
+answer:1
+},
+
+{
+question:"Which keyword declares a variable in JavaScript?",
+options:[
+"var",
+"int",
+"string",
+"define"
+],
+answer:0
+},
+
+{
+question:"Which file extension is used for JavaScript files?",
 options:[
 ".html",
 ".css",
@@ -43,71 +109,303 @@ options:[
 ],
 answer:2
 },
+    {
+
+question:"Which programming language is known as platform independent?",
+
+options:[
+"Java",
+"C",
+"HTML",
+"SQL"
+],
+
+answer:0
+
+},
 
 {
-question:"SQL ka use kis liye hota hai?",
+
+question:"Which keyword is used to create an object in Java?",
+
+options:[
+"class",
+"new",
+"object",
+"create"
+],
+
+answer:1
+
+},
+
+{
+
+question:"Which method is the entry point of a Java program?",
+
+options:[
+"start()",
+"main()",
+"run()",
+"init()"
+],
+
+answer:1
+
+},
+
+{
+
+question:"Which data type stores whole numbers in Java?",
+
+options:[
+"float",
+"boolean",
+"int",
+"char"
+],
+
+answer:2
+
+},
+
+{
+
+question:"OOP stands for?",
+
+options:[
+"Object Oriented Programming",
+"Open Operating Program",
+"Object Online Process",
+"Output Oriented Programming"
+],
+
+answer:0
+
+},
+
+{
+
+question:"Which loop runs while a condition is true?",
+
+options:[
+"if loop",
+"while loop",
+"switch loop",
+"case loop"
+],
+
+answer:1
+
+},
+
+{
+
+question:"Which symbol is used for multiplication in programming?",
+
+options:[
+"x",
+"*",
+"#",
+"%"
+],
+
+answer:1
+
+},
+
+{
+
+question:"Which concept allows one class to acquire properties of another class?",
+
+options:[
+"Encapsulation",
+"Inheritance",
+"Compilation",
+"Iteration"
+],
+
+answer:1
+
+},
+
+{
+
+question:"Which language is mainly used for Android development with Java?",
+
+options:[
+"Kotlin",
+"HTML",
+"SQL",
+"CSS"
+],
+
+answer:0
+
+},
+
+{
+
+question:"A compiler converts source code into?",
+
 options:[
 "Images",
-"Database Management",
-"Design",
-"Animation"
-],
-answer:1
-},
-
-{
-question:"GitHub kis liye use hota hai?",
-options:[
-"Code Hosting",
-"Gaming",
-"Editing",
-"Photos"
-],
-answer:0
-},
-
-{
-question:"React kya hai?",
-options:[
+"Machine code",
 "Database",
-"JavaScript Library",
-"Browser",
-"Language"
+"Text file"
 ],
+
 answer:1
+
+},
+    {
+
+question:"SQL stands for?",
+
+options:[
+"Structured Query Language",
+"Simple Query Language",
+"System Query Logic",
+"Standard Question Language"
+],
+
+answer:0
+
 },
 
 {
-question:"Java kis company ne banayi?",
+
+question:"Which SQL command is used to retrieve data?",
+
 options:[
-"Microsoft",
-"Sun Microsystems",
-"Google",
-"Apple"
+"GET",
+"SELECT",
+"FETCHALL",
+"SHOW"
 ],
+
 answer:1
+
 },
 
 {
-question:"Responsive website ka matlab?",
+
+question:"Which command is used to add new data into a table?",
+
 options:[
-"Fast Website",
-"Mobile Friendly Website",
-"Database Website",
-"Offline Website"
+"INSERT",
+"UPDATE",
+"ADD",
+"CREATE"
 ],
-answer:1
+
+answer:0
+
 },
 
 {
-question:"Portfolio website kis liye hoti hai?",
+
+question:"Which database is a relational database?",
+
 options:[
-"Games",
-"Show Skills & Projects",
-"Movies",
-"Shopping"
+"MySQL",
+"HTML",
+"CSS",
+"JavaScript"
 ],
+
+answer:0
+
+},
+
+{
+
+question:"Primary key is used to?",
+
+options:[
+"Delete database",
+"Uniquely identify records",
+"Create website",
+"Design page"
+],
+
 answer:1
+
+},
+
+{
+
+question:"Which SQL command modifies existing records?",
+
+options:[
+"CHANGE",
+"UPDATE",
+"MODIFY TABLE",
+"EDIT"
+],
+
+answer:1
+
+},
+
+{
+
+question:"DBMS stands for?",
+
+options:[
+"Database Management System",
+"Data Building Management System",
+"Digital Base Management Software",
+"Database Machine System"
+],
+
+answer:0
+
+},
+
+{
+
+question:"Which normal form removes repeating groups of data?",
+
+options:[
+"1NF",
+"2NF",
+"3NF",
+"4NF"
+],
+
+answer:0
+
+},
+
+{
+
+question:"Which language is used to manage databases?",
+
+options:[
+"SQL",
+"HTML",
+"CSS",
+"XML"
+],
+
+answer:0
+
+},
+
+{
+
+question:"What is a database?",
+
+options:[
+"A collection of organized data",
+"A programming language",
+"A website design",
+"A computer game"
+],
+
+answer:0
+
 }
 
 ];
