9da8ae735fc4d9eccb7096721e6eea35c1bee36d
Muhammad ikram ullah <ikramahmed12201@gmail.com>
2026-07-28 03:58:06 -0700

    Update script.js

--- ecommerce-store/ css/js/script.js
+++ ecommerce-store/ css/js/script.js
@@ -1,1 +1,53 @@
+const products = [
 
+{
+    name:"Wireless Headphones",
+    price:"$99",
+    image:"https://picsum.photos/400/400?random=1"
+},
+
+{
+    name:"Smart Watch",
+    price:"$149",
+    image:"https://picsum.photos/400/400?random=2"
+},
+
+{
+    name:"Gaming Mouse",
+    price:"$59",
+    image:"https://picsum.photos/400/400?random=3"
+},
+
+{
+    name:"Bluetooth Speaker",
+    price:"$89",
+    image:"https://picsum.photos/400/400?random=4"
+}
+
+];
+
+const container = document.getElementById("products-container");
+
+products.forEach(product => {
+
+container.innerHTML += `
+
+<div class="product-card">
+
+<img src="${product.image}" alt="${product.name}">
+
+<div class="product-info">
+
+<h3>${product.name}</h3>
+
+<p class="price">${product.price}</p>
+
+<button>Add to Cart</button>
+
+</div>
+
+</div>
+
+`;
+
+});
