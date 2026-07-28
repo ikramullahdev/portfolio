b1783747697326acb379241042ab09405a45d908
Muhammad ikram ullah <ikramahmed12201@gmail.com>
2026-07-28 05:14:13 -0700

    Implement quantity adjustment for cart items
    Added quantity management for cart items and updated total display.

--- ecommerce-store/js/cart.js
+++ ecommerce-store/js/cart.js
@@ -3,6 +3,12 @@ const total = document.getElementById("total");
 
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
+// Quantity add agar pehle se nahi hai
+cart = cart.map(item => ({
+    ...item,
+    quantity: item.quantity || 1
+}));
+
 function loadCart(){
 
     cartItems.innerHTML = "";
@@ -11,19 +17,29 @@ function loadCart(){
 
     cart.forEach((item,index)=>{
 
-        grandTotal += item.price;
+        grandTotal += item.price * item.quantity;
 
         cartItems.innerHTML += `
 
         <div class="product-card">
 
-            <img src="${item.image}" alt="">
+            <img src="${item.image}" alt="${item.name}">
 
             <div class="product-info">
 
                 <h3>${item.name}</h3>
 
                 <p class="price">$${item.price}</p>
+
+                <div class="qty">
+
+                    <button onclick="decrease(${index})">-</button>
+
+                    <span>${item.quantity}</span>
+
+                    <button onclick="increase(${index})">+</button>
+
+                </div>
 
                 <button onclick="removeItem(${index})">
                     Remove
@@ -37,18 +53,26 @@ function loadCart(){
 
     });
 
-    total.innerHTML = "$" + grandTotal;
+    total.innerHTML = "$" + grandTotal.toFixed(2);
 
+    localStorage.setItem("cart", JSON.stringify(cart));
 }
 
 loadCart();
 
-function removeItem(index){
-
-    cart.splice(index,1);
-
-    localStorage.setItem("cart",JSON.stringify(cart));
-
+function increase(index){
+    cart[index].quantity++;
     loadCart();
+}
 
+function decrease(index){
+    if(cart[index].quantity > 1){
+        cart[index].quantity--;
+    }
+    loadCart();
+}
+
+function removeItem(index){
+    cart.splice(index,1);
+    loadCart();
 }
