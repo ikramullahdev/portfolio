47fa52f274bc55cad7bf91ca9b2fa1204478a64e
Muhammad ikram ullah <ikramahmed12201@gmail.com>
2026-07-28 05:09:59 -0700

    Add showToast function to display toast notifications

--- ecommerce-store/js/products.js
+++ ecommerce-store/js/products.js
@@ -150,3 +150,16 @@ function updateCartCount(){
 }
 
 updateCartCount();
+function showToast(){
+
+    const toast = document.getElementById("toast");
+
+    toast.classList.add("show");
+
+    setTimeout(()=>{
+
+        toast.classList.remove("show");
+
+    },2000);
+
+}
