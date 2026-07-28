7b8bceda57830759a592a7016cabea7a83ff9f3e
Muhammad ikram ullah <ikramahmed12201@gmail.com>
2026-07-28 06:40:30 -0700

    Add wishlist functionality to wishlist.js

--- ecommerce-store/js/wishlist.js
+++ ecommerce-store/js/wishlist.js
@@ -1,1 +1,20 @@
+const wishlistBtn = document.getElementById("wishlist-btn");
 
+if(wishlistBtn){
+
+    wishlistBtn.addEventListener("click", function(){
+
+        let wishlist = JSON.parse(localStorage.getItem("wishlist")) || [];
+
+        wishlist.push(product);
+
+        localStorage.setItem(
+            "wishlist",
+            JSON.stringify(wishlist)
+        );
+
+        alert(product.name + " added to wishlist ❤️");
+
+    });
+
+}
