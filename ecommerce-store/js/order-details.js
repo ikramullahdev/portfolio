1d4d76a0543bfb4b7e9ac7ef83d7c0bd2c743b06
Muhammad ikram ullah <ikramahmed12201@gmail.com>
2026-07-29 04:49:57 -0700

    Create order-details.js

---
+++ ecommerce-store/js/order-details.js
@@ -0,0 +1,113 @@
+const container = document.getElementById("order-details");
+
+
+
+const selectedOrder =
+JSON.parse(localStorage.getItem("selectedOrder"));
+
+
+
+if(selectedOrder){
+
+
+container.innerHTML = `
+
+
+<div class="product-card">
+
+
+<div class="product-info">
+
+
+<h3>
+Order ID: ${selectedOrder.id}
+</h3>
+
+
+<p>
+Date: ${selectedOrder.date}
+</p>
+
+
+<p>
+Status: ${selectedOrder.status}
+</p>
+
+
+<h3>
+Products
+</h3>
+
+
+${selectedOrder.items.map(item=>`
+
+<p>
+${item.name} x ${item.quantity || 1}
+</p>
+
+`).join("")}
+
+
+
+<h2>
+Total: $${selectedOrder.total}
+</h2>
+
+
+
+<button onclick="cancelOrder()"
+class="wishlist-btn">
+
+Cancel Order
+
+</button>
+
+
+
+</div>
+
+
+</div>
+
+
+`;
+
+
+
+}
+
+
+
+
+
+function cancelOrder(){
+
+
+let orders =
+JSON.parse(localStorage.getItem("orders")) || [];
+
+
+
+orders = orders.filter(order=>
+
+order.id !== selectedOrder.id
+
+);
+
+
+
+localStorage.setItem(
+"orders",
+JSON.stringify(orders)
+);
+
+
+
+alert("Order cancelled successfully");
+
+
+
+window.location.href="orders.html";
+
+
+}
